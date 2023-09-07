fac507eb500d78425d9b8c66b71d4c4b5e1a94bf
NicolasEQuintana <nicolasequintana@gmail.com>
2023-09-07 15:53:12 -0300

    commit repository

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+});
